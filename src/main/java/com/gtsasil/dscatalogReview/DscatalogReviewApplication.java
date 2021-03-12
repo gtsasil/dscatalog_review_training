4099d89c75fa27d10fa053a655892b31606f4ad8
Gleisson Santos Silva <gtsasil@gmail.com>
2021-03-12 13:47:14 +0000

    First commit

---
+++ src/main/java/com/gtsasil/dscatalogReview/DscatalogReviewApplication.java
@@ -0,0 +1,13 @@
+package com.gtsasil.dscatalogReview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatalogReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatalogReviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gtsasil/dscatalogReview/DscatalogReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtsasil.dscatalogReview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
